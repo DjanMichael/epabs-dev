7d8a6b51ec4827d6884bca6256380ff15e1177eb
Djan Michael <djan.pengson@gmail.com>
2020-09-11 00:39:44 +0800

    code split table modal pi table

--- resources/views/pages/transaction/wfp/create_wfp.blade.php
+++ resources/views/pages/transaction/wfp/create_wfp.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 @section('title','CREATE WFP')
+@section('breadcrumb')
+<li class="breadcrumb-item">
+    <a href="{{ route('r_wfp') }}" class="text-muted">WFP</a>
+</li>
+<li class="breadcrumb-item">
+    <a href="{{ route('r_create_wfp') }}" class="text-muted">Create</a>
+</li>
+@endsection
 @section('content')
 <div class="card card-custom gutter-b example example-compact">
     <div class="card-header">
@@ -228,63 +236,12 @@
                             <i class="flaticon2-add-1"></i> Add Performance Indicator
                         </a>      
                     </div>
-                    <div class="col-12">
-                        <div class="table-responsive">
-                            <table class="table table-sm table-hover" class="wfp_table">
-                                <thead style="text-align:center;" class="bg-dark">
-                                    <tr style="color:white">
-                                        <th scope="col">Action</th>
-                                        <th scope="col">PI</th>
-                                        <th scope="col">UACS Code</th>
-                                        <th scope="col">Performance Indicator</th>
-                                        <th scope="col">Cost</th>
-                                        <th scope="col">PPMP</th>
-                                        <th scope="col">Catering</th>
-                                        <th scope="col">Batch</th>
-                                        <th scope="col">Budget Line Item #</th>
-                                        
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr class="wfp_table_row">
-                                        <td scope="row">
-                                           <div class="col-12">
-                                            <button class="btn btn-success btn-sm">  <i class="flaticon-edit"></i> Edit</button>
-                                           </div>
-                                           <div class="col-12 mt-1">
-                                            <button class="btn btn-danger btn-sm">  <i class="flaticon-delete-1"></i> Delete</button>
-                                           </div>
-                                        </td>
-                                        <td>00041292</td>
-                                        <td>5001356562</td>
-                                        <td>asdas das dasdasdasdasdasdasdasdasd asd asd a ad asda as eeg ge ge sgsdgdsdsf sf dsfdsfdsf dsf sfsdfdss sdf sfssd sd s sdfsdf </td>
-                                        <td>3,400,122.00</td>
-                                        <td>12</td>
-                                        <td>123</td>
-                                        <td>412</td>
-                                        <td>412</td>
-                                        
-                                    </tr>
-                                    <tr class="wfp_table_row">
-                                        <td scope="row">1</td>
-                                        <td>00041292</td>
-                                        <td>5001356562</td>
-                                        <td>asdas das dasdasdasdasdasdasdasdasd asd asd a ad asda as eeg ge ge sgsdgdsdsf sf dsfdsfdsf dsf sfsdfdss sdf sfssd sd s sdfsdf </td>
-                                        <td>1,000,100.00</td>
-                                        <td>12</td>
-                                        <td>123</td>
-                                        <td>412</td>
-                                        <td>412</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                    <div class="col-12" id="peformance_indicator_table_content">
                         <div class="col-12 p-20 w-100" style="height:150px; text-align:center;border:1px solid grey;">
                             <i class="flaticon2-start-up icon-2x"></i>
                             <p>No Indicator</p>
                         </div>
                       </div>
-                  
                   </div>
                </div>
            </div>
