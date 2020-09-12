fdb21a233a7676c66b21c6fabc58aa7cde7e741a
Djan Michael <djan.pengson@gmail.com>
2020-09-12 14:24:11 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3108,16 +3108,6 @@ License: You must have a valid license purchased only from themeforest(the above
                                 }
                         });
                         
-                       $(document).ready(){
-                        setInterval(function(){
-                                console.log(1);
-                                if(navigator.onLine){
-                                alert('online');
-                                } else {
-                                alert('offline');
-                                }
-                            },1000)
-                       }
                     </script>
             </body>
     <!--end::Body-->
