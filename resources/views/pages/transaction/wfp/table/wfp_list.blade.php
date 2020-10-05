51fe0ff97512cb0c807121987e69dd0872c211b9
Djan Michael <djan.pengson@gmail.com>
2020-10-06 00:08:12 +0800

    wfp_list init
    wfp_list init

---
+++ resources/views/pages/transaction/wfp/table/wfp_list.blade.php
@@ -0,0 +1,87 @@
+
+<div class="d-flex flex-column-fluid">
+    <!--begin::Container-->
+    <div class="container">
+        <!--begin::Row-->
+        <div class="row">
+            <div class="col-xl-6">
+                <!--begin::Card-->
+                <div class="card card-custom gutter-b card-stretch  ribbon ribbon-clip ribbon-right" onclick="wfp_drawer_open(1)" style="cursor:pointer;" id="wfp_card">
+                    <div class="ribbon-target" style="top: 12px;">
+                        <span class="ribbon-inner bg-warning"></span>Status
+                       </div>
+                    <!--begin::Body-->
+                    <div class="card-body">
+                        <!--begin::Section-->
+                        <div class="row">
+                            <div class="col-12 d-flex align-items-center">
+                                <div class="symbol-list d-flex flex-wrap">
+                                    <div class="symbol symbol-60 mr-3">
+                                        <span class="symbol-label font-size-h6">P</span>
+                                    </div>
+
+                                </div>
+                                <!--end::Pic-->
+                                <!--begin::Info-->
+                                <div class="d-flex flex-column mr-auto">
+                                    <!--begin: Title-->
+                                    <a href="#" class="card-title text-hover-primary font-weight-bolder font-size-h5 text-dark mb-1">DJAN MICAHEL ANTHONY A. PENGSON</a>
+                                    <span class="text-muted font-weight-bold">PROGRAMMER</span>
+                                    <!--end::Title-->
+                                </div>
+                            </div>
+                        </div>
+                        <div class="separator separator-dashed separator-border-2 separator-secondary mb-3 mt-6"></div>
+                        <!--end::Section-->
+                        <!--begin::Blog-->
+                        <div class="d-flex flex-wrap">
+                            <!--begin: Item-->
+                            <div class="mr-12 d-flex flex-column mb-7">
+                                <span class="font-weight-bolder mb-4">Cost</span>
+                                <span class="font-weight-bolder font-size-h5 pt-1">
+                                <span class="font-weight-bold text-dark-50">Php</span>1,249,500</span>
+                            </div>
+                            <!--end::Item-->
+                            <!--begin::Item-->
+                            <div class="mr-12 d-flex flex-column mb-7">
+                                <span class="font-weight-bolder mb-4">Budget Source</span>
+                                <span class="font-weight-bolder font-size-h5 pt-1">
+                                <span class="font-weight-bold text-dark-50"></span>NEP</span>
+                            </div>
+                            <!--end::Item-->
+                               <!--begin::Item-->
+                               <div class="mr-12 d-flex flex-column mb-7">
+                                <span class="font-weight-bolder mb-4">Budget Year</span>
+                                <span class="font-weight-bolder font-size-h5 pt-1">
+                                <span class="font-weight-bold text-dark-50"></span>2020</span>
+                            </div>
+                            <!--end::Item-->
+                            <!--begin::Item-->
+                            <div class="mr-12 d-flex flex-column mb-7">
+                                <span class="font-weight-bolder mb-4">No. of Activities</span>
+                                <span class="font-weight-bolder font-size-h5 pt-1">
+                                <span class="font-weight-bold text-dark-50"></span>17</span>
+                            </div>
+                            <!--end::Item-->
+                        </div>
+
+                        <!--end::Blog-->
+                    </div>
+                    <!--end::Body-->
+                    <!--begin::Footer-->
+                    <div class="card-footer d-flex align-items-center">
+                        <div class="d-flex">
+                            <div class="d-flex align-items-center mr-7">
+                                <span class="label label-primary label-inline font-weight-bolder mr-2" >MAIP</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end::Footer-->
+                </div>
+                <!--end::Card-->
+            </div>
+        </div>
+        <!--end::Row-->
+    </div>
+    <!--end::Container-->
+</div>
