bbabb9766109f351ff076aea35cfd21f5a2e288c
Djan Michael <djan.pengson@gmail.com>
2020-11-23 11:59:38 +0800

    fix bug

--- app/User.php
+++ app/User.php
@@ -51,7 +51,7 @@ class User extends Authenticatable
 
     public function getSelectedProgramId(){
         $res = GlobalSystemSettings::where('user_id',Auth::user()->id)->first();
-        return $res->select_program_id;
+        return $res != null ? $res->select_program_id : null;
     }
 
 
