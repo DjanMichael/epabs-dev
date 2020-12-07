0566cf650145d397d0a845193810045ed7889cee
John Paul Ortiz <jitzu123@gmail.com>
2020-12-07 11:51:32 +0800

    remove doh_epabs

--- database/migrations/2020_11_01_233725_create_view_get_all_year_budget_per_unit_dataset.php
+++ database/migrations/2020_11_01_233725_create_view_get_all_year_budget_per_unit_dataset.php
@@ -46,7 +46,7 @@ class CreateViewGetAllYearBudgetPerUnitDataset extends Migration
                         SELECT
                             sum(`tuba`.`program_budget`)
                         FROM
-                            `doh_epabs`.`tbl_unit_budget_allocation` `tuba`
+                            `tbl_unit_budget_allocation` `tuba`
                         WHERE
                             `tuba`.`year_id` = `dataset2`.`year_id`
                         AND `tuba`.`program_id` = `dataset2`.`program_id`
@@ -73,7 +73,7 @@ class CreateViewGetAllYearBudgetPerUnitDataset extends Migration
                                     SELECT
                                         count(0)
                                     FROM
-                                        `doh_epabs`.`tbl_unit_budget_allocation` `tuba`
+                                        `tbl_unit_budget_allocation` `tuba`
                                     WHERE
                                         `tuba`.`unit_id` = `t1_unit_id`
                                     AND `tuba`.`year_id` = `t1_year_id`
@@ -84,21 +84,21 @@ class CreateViewGetAllYearBudgetPerUnitDataset extends Migration
                                         (
                                             (
                                                 (
-                                                    `doh_epabs`.`tbl_unit_program` `tup`
-                                                    JOIN `doh_epabs`.`ref_year` `ry` ON (`ry`.`year` <> `tup`.`id`)
+                                                     `tbl_unit_program` `tup`
+                                                    JOIN `ref_year` `ry` ON (`ry`.`year` <> `tup`.`id`)
                                                 )
-                                                JOIN `doh_epabs`.`ref_units` `ru` ON (`ru`.`id` = `tup`.`unit_id`)
+                                                JOIN `ref_units` `ru` ON (`ru`.`id` = `tup`.`unit_id`)
                                             )
-                                            JOIN `doh_epabs`.`users` `u` ON (`u`.`id` = `tup`.`user_id`)
+                                            JOIN `users` `u` ON (`u`.`id` = `tup`.`user_id`)
                                         )
-                                        JOIN `doh_epabs`.`users_profile` `up` ON (`up`.`user_id` = `u`.`id`)
+                                        JOIN `users_profile` `up` ON (`up`.`user_id` = `u`.`id`)
                                     )
-                                    JOIN `doh_epabs`.`ref_program` `rp` ON (
+                                    JOIN `ref_program` `rp` ON (
                                         `rp`.`id` = `tup`.`program_id`
                                     )
                                 )
                         ) `dataset1`
-                        LEFT JOIN `doh_epabs`.`vw_unit_budget_allocation_utilization` `dataset2` ON (
+                        LEFT JOIN  `vw_unit_budget_allocation_utilization` `dataset2` ON (
                             `dataset2`.`program_id` = `dataset1`.`t1_program_id`
                             AND `dataset2`.`year_id` = `dataset1`.`t1_year_id`
                         )
@@ -108,7 +108,7 @@ class CreateViewGetAllYearBudgetPerUnitDataset extends Migration
                         SELECT
                             `tbr`.`role_id`
                         FROM
-                            `doh_epabs`.`tbl_user_roles` `tbr`
+                             `tbl_user_roles` `tbr`
                         WHERE
                             `tbr`.`roles` = "PROGRAM COORDINATOR"
                     )
