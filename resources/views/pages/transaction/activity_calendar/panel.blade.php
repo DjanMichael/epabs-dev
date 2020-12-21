1e3f2be4174a268244462b6928c5dacfc3ad9391
John Paul Ortiz <jitzu123@gmail.com>
2020-12-22 02:56:13 +0800

    updates-activity-calendar

--- resources/views/pages/transaction/activity_calendar/panel.blade.php
+++ resources/views/pages/transaction/activity_calendar/panel.blade.php
@@ -5,10 +5,10 @@
         <div class="card card-custom gutter-b">
             <div class="card-header">
                 <div class="card-title">
-                    <span class="card-icon"><i class="@yield('panel-icon') text-primary"></i></span>
-                    <h3 class="card-label">@yield('panel-title')</h3>
+                    <span class="card-icon"><i class="flaticon-event-calendar-symbol text-primary"></i></span>
+                    <h3 class="card-label">Calendar List View</h3>
                 </div>
-                <div class="card-toolbar">No Year Selected</div>
+                <div class="card-toolbar"><b>{{ $data['year'] }}</b></div>
             </div>
             <div class="card-body">
                 <div class="accordion" id="accordionExample1">
@@ -22,16 +22,32 @@
                             <div id="collapse{{ $key }}" class="collapse show" data-parent="#accordionExample1">
                                 <div class="card-body">
                                     <table style="width:100%">
-                                        <tr>
-                                            <td style="width: 20%;">Code: </td>
-                                            <td style="width: 60%;">Title: </td>
-                                            <td style="width: 20%;">Cost: </td>
-                                        </tr>
-                                        <tr>
-                                            <td style="width: 20%;"></td>
-                                            <td style="width: 60%;">Point Person: </td>
-                                            <td style="width: 20%;"></td>
-                                        </tr>
+                                        @forelse ($data['activity_list'] as $row)
+                                            @if ($row['activity_timeframe'] == $value)
+                                                <tr>
+                                                    <td style="width: 20%;">Code: <b>{{ $row["code"] }}<b></td>
+                                                    <td style="width: 5%;"><span class="fas fa-dot-circle text-success mr-5"></span></td>
+                                                    <td style="width: 55%;">Title: <b>{{ $row["out_activity"] }}</b></td>
+                                                    <td style="width: 20%;">Cost: <b>{{ $row["activity_cost"] }}<b></td>
+                                                </tr>
+                                                <tr>
+                                                    <td style="width: 20%;"></td>
+                                                    <td style="width: 5%;"></td>
+                                                    <td style="width: 55%;">Point Person: <b>{{ $row["responsible_person"] }}</b></td>
+                                                    <td style="width: 20%;"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td style="width: 20%;"><hr></td>
+                                                    <td style="width: 5%;"><hr></td>
+                                                    <td style="width: 55%;"><hr></td>
+                                                    <td style="width: 20%;"><hr></td>
+                                                </tr>
+                                            @endif
+                                        @empty
+                                            <tr>
+                                                <td colspan="12" class="text-center"> NO DATA AVAILABLE IN TABLE</td>
+                                            </tr>
+                                        @endforelse
                                     </table>
                                 </div>
                             </div>
