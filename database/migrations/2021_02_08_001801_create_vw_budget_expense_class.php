8621af9ed1f5bd4296e95004f941f9f7b3fe47a7
Djan Michael <djan.pengson@gmail.com>
2021-02-08 00:21:13 +0800

    Create 2021_02_08_001801_create_vw_budget_expense_class.php

---
+++ database/migrations/2021_02_08_001801_create_vw_budget_expense_class.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use DB as DB2;
+class CreateVwBudgetExpenseClass extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB2::statement('
+        CREATE VIEW  vw_budget_expense_class AS (
+            (
+                SELECT
+                    sum(`ds1`.`amount`) AS `total`,
+                    `ds1`.`category` AS `category`,
+                    `ds1`.`year_id` AS `year_id`
+                FROM
+                    (
+                        SELECT
+                            `twapi`.`wfp_code` AS `wfp_code`,
+                            trim(
+                                LEADING "0"
+                                FROM
+                                    substr(`twapi`.`wfp_code`, 7, 3)
+                            ) AS `year_id`,
+                            `twapi`.`uacs_id` AS `uacs_id`,
+                            sum(`twapi`.`cost`) AS `amount`,
+                            `ru`.`category` AS `category`,
+                            `twapi`.`id` AS `id`
+                        FROM
+                            (
+                                `tbl_wfp_activity_per_indicator` `twapi`
+                                JOIN `ref_uacs` `ru` ON (
+                                    `ru`.`code` = `twapi`.`uacs_id`
+                                )
+                            )
+                        GROUP BY
+                            `twapi`.`id`,
+                            trim(
+                                LEADING "0"
+                                FROM
+                                    substr(`twapi`.`wfp_code`, 7, 3)
+                            )
+                    ) `ds1`
+                GROUP BY
+                    `ds1`.`id`,
+                    `ds1`.`category`,
+                    `ds1`.`year_id`
+
+        )');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vw_budget_expense_class');
+    }
+}
