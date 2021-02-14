872501019f793485fb9fea68532e1f463e9a12eb
Djan Michael <djan.pengson@gmail.com>
2021-02-15 00:10:50 +0800

    Update 2020_08_24_066046_create_vw_budget_line_item.php

--- database/migrations/2020_08_24_066046_create_vw_budget_line_item.php
+++ database/migrations/2020_08_24_066046_create_vw_budget_line_item.php
@@ -22,21 +22,21 @@ class CreateVwBudgetLineItem extends Migration
                 rbli.fund_source_id,
                 rsof.sof_classification,
                 rbli.budget_item,
-                rbli.unit_program_id,
-                vup.program_id,
                 rp.program_name,
+                ru.division,
                 rbli.year_id,
                 ry.`year`,
                 rbli.allocation_amount,
                 rbli.saa_ctrl_number,
                 rbli.purpose,
-                rbli.`status` 
-            FROM	
+                rbli.`status`
+            FROM
                 ref_budget_line_item rbli
                 LEFT JOIN ref_year ry ON ry.id = rbli.year_id
-                LEFT JOIN ref_source_of_fund rsof ON rsof.id = rbli.fund_source_id 
-                LEFT JOIN vw_unit_program vup ON vup.id = rbli.unit_program_id 
-                LEFT JOIN ref_program rp ON rp.id = vup.program_id 
+                LEFT JOIN ref_source_of_fund rsof ON rsof.id = rbli.fund_source_id
+                JOIN tbl_unit_budget_allocation tuba ON tuba.budget_line_item_id = rbli.id
+                JOIN ref_program rp ON rp.id = tuba.program_id
+                JOIN ref_units ru ON ru.id = tuba.unit_id
             )
         ');
     }
