af1af76d024a4a10f6421fb1ded9440f23e21e4e
John Paul Ortiz <jitzu123@gmail.com>
2021-03-28 23:23:33 +0800

    Update 2020_08_24_066046_create_vw_budget_line_item.php

--- database/migrations/2020_08_24_066046_create_vw_budget_line_item.php
+++ database/migrations/2020_08_24_066046_create_vw_budget_line_item.php
@@ -17,7 +17,30 @@ class CreateVwBudgetLineItem extends Migration
         //
         DB2::statement('
             CREATE VIEW vw_budget_line_item AS (
-                (select `rbli`.`id` AS `id`,`rbli`.`fund_source_id` AS `fund_source_id`,`rsof`.`sof_classification` AS `sof_classification`,`rbli`.`budget_item` AS `budget_item`,`rbli`.`program_id` AS `program_id`,`rp`.`program_name` AS `program_name`,`rbli`.`unit_id` AS `unit_id`,`ru`.`division` AS `division`,`ru`.`section` AS `section`,`rbli`.`year_id` AS `year_id`,`ry`.`year` AS `year`,`tuba`.`program_budget` AS `allocation_amount`,`rbli`.`saa_ctrl_number` AS `saa_ctrl_number`,`rbli`.`purpose` AS `purpose`,`rbli`.`status` AS `status` from (((((`ref_budget_line_item` `rbli` left join `ref_year` `ry` on(`ry`.`id` = `rbli`.`year_id`)) left join `ref_source_of_fund` `rsof` on(`rsof`.`id` = `rbli`.`fund_source_id`)) left join `tbl_unit_budget_allocation` `tuba` on(`tuba`.`budget_line_item_id` = `rbli`.`id`)) left join `ref_program` `rp` on(`rp`.`id` = `tuba`.`program_id`)) left join `ref_units` `ru` on(`ru`.`id` = `tuba`.`unit_id`)) order by `rbli`.`id`)
+                SELECT
+                    `rbli`.`id` AS `id`,
+                    `rbli`.`fund_source_id` AS `fund_source_id`,
+                    `rsof`.`sof_classification` AS `sof_classification`,
+                    `rbli`.`budget_item` AS `budget_item`,
+                    `rbli`.`program_id` AS `program_id`,
+                    `rp`.`program_name` AS `program_name`,
+                    `rbli`.`unit_id` AS `unit_id`,
+                    `ru`.`division` AS `division`,
+                    `ru`.`section` AS `section`,
+                    `rbli`.`year_id` AS `year_id`,
+                    `ry`.`year` AS `year`,
+                    `rbli`.`allocation_amount` AS `allocation_amount`,
+                    `rbli`.`saa_ctrl_number` AS `saa_ctrl_number`,
+                    `rbli`.`purpose` AS `purpose`,
+                    `rbli`.`status` AS `status` 
+                FROM
+                    `ref_budget_line_item` `rbli` 
+                    LEFT JOIN `ref_year` `ry` ON ( `ry`.`id` = `rbli`.`year_id` ) 
+                    LEFT JOIN `ref_source_of_fund` `rsof` ON ( `rsof`.`id` = `rbli`.`fund_source_id` ) 
+                    LEFT JOIN `ref_program` `rp` ON ( `rp`.`id` = `rbli`.`program_id` ) 
+                    LEFT JOIN `ref_units` `ru` ON ( `ru`.`id` = `rbli`.`unit_id` )
+                ORDER BY
+                    `rbli`.`id` 
             )
         ');
     }
